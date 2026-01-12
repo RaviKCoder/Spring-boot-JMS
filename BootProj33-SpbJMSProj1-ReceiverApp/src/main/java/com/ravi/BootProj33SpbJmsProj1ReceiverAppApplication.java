27e8416f149a421df9ba307b30613040331da2d0
Ravi kumar <rsravi433399@gmail.com>
2026-01-13 03:30:29 +0530

    1st proj added

---
+++ BootProj33-SpbJMSProj1-ReceiverApp/src/main/java/com/ravi/BootProj33SpbJmsProj1ReceiverAppApplication.java
@@ -0,0 +1,13 @@
+package com.ravi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj33SpbJmsProj1ReceiverAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj33SpbJmsProj1ReceiverAppApplication.class, args);
+	}
+
+}
---
+++ BootProj33-SpbJMSProj1-SenderApp/src/main/java/com/ravi/BootProj33SpbJmsProj1SenderAppApplication.java
@@ -0,0 +1,13 @@
+package com.ravi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj33SpbJmsProj1SenderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj33SpbJmsProj1SenderAppApplication.class, args);
+	}
+
+}
